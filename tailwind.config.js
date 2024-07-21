69f92b7dede1678d9d12d6d80ce7731024317f58
Edi Edwardson C. Meer <edison.cmeer@gmail.com>
2024-07-22 00:16:36 +0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: '#E0F7FA',
+        softTeal: '#B2DFDB',
+        paleCoral: '#FFCDD2',
+        whiteSmoke: '#F5F5F5',
+        charcoal: '#37474F',
+        lightGold: '#FFF9C4',
+        matteBlack: '#28282b',
+      },
+    },
+  },
+  plugins: [],
+}
